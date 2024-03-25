0aa113b8d4772f6c082ef8611e1b4e621c35c90c
dong.qi <42195979+qidongde@users.noreply.github.com>
2024-03-25 03:10:52 -0500

    Update seq2seq_att.py

--- seq2seq_att.py
+++ seq2seq_att.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
 import torch.optim as optim
+from tqdm import tqdm
 import time
 
 import random
@@ -13,6 +14,15 @@ import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 batch_size = 16
+hidden_size = 50
+epochs = 20
+mylr = 1e-4
+dropout_p = 0.1
+
+
+def train_test_split_func():
+    train_pairs, test_pairs = []
+    return train_pairs, test_pairs
 
 
 # raw_data -> MyPairsDataset --> DataLoader
@@ -62,15 +72,17 @@ class EncoderRNN(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
 
+        self.embedding = nn.Linear(input_size, hidden_size)
         self.gru = nn.GRU(input_size, hidden_size, batch_first=True)
 
     def forward(self, input, hidden):
         # input[batch_size,time_step,input_size]
-        output = self.embedding(input)
+        # --> input_[batch_size,time_step,hidden_size]
+        input_ = F.relu(self.embedding(input))
 
-        # gru([batch_size,time_step,input_size],[num_layers,batch_size,hidden_size])
-        # --> [batch_size,time_step,input_size] [num_layers,batch_size,hidden_size]
-        output, hidden = self.gru(output, hidden)
+        # gru([batch_size,time_step,hidden_size],[num_layers,batch_size,hidden_size])
+        # --> [batch_size,time_step,hidden_size] [num_layers,batch_size,hidden_size]
+        output, hidden = self.gru(input_, hidden)
         return output, hidden
 
     def inithidden(self):
@@ -101,7 +113,7 @@ class AttnDecoderRNN(nn.Module):
         # encoder_outputs(k): [batch_size,seq_length,hidden_size]
 
         # embedded: [batch_size,1,input_size] --> [batch_size,1,hidden_size]
-        embedded = self.embedding(input)
+        embedded = F.relu(self.embedding(input))
         embedded = self.dropout(embedded)
 
         # attn1_weights: [batch_size,seq_length]
@@ -134,5 +146,75 @@ class AttnDecoderRNN(nn.Module):
         return torch.zeros(1, batch_size, self.hidden_size, device=device)
 
 
+def Train_Iters(x, y, my_encoderrnn, my_attndecoderrnn, myadam_encode, myadam_decode, mse_loss):
+    # encode_output, encode_hidden = my_encoderrnn(x, encode_hidden)
+    encode_hidden = my_encoderrnn.inithidden()
+    encode_output, encode_hidden = my_encoderrnn(x, encode_hidden)
+    # [batch_size,121,25],[1,batch_size,hidden_size]
+    # --> [batch_size,121,hidden_size],[1,batch_size,hidden_size]
+    # encode_output_c [batch_size,121,hidden_size]
+
+    decode_hidden = encode_hidden
+
+    myloss = 0.0
+    input_y = torch.zeros((batch_size, 1, decode_hidden), device=device)
+    y_len = y.shape[1]
+    y_pre = torch.zeros(batch_size, y_len, device=device)
+    for idx in range(y_len):
+        output_y, decode_hidden, attn_weight = my_attndecoderrnn(input_y, decode_hidden, encode_output)
+        y_pre[:, idx] = output_y
+        y_true = y[:, idx]
+        myloss += mse_loss(output_y, y_true)
+        input_y = output_y.detach()
+
+    myadam_encode.zero_grad()
+    myadam_decode.zero_grad()
+
+    myloss.backward()
+
+    myadam_encode.step()
+    myadam_decode.step()
+
+    return y_pre, y
+
+
+def Train_seq2seq():
+    train_pairs, test_pairs = train_test_split_func()
+
+    train_dataset = MyPairsDataset(train_pairs)
+    test_dataset = MyPairsDataset(test_pairs)
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+
+    my_encoderrnn = EncoderRNN(25, hidden_size)
+    my_attndecoderrnn = AttnDecoderRNN(hidden_size, hidden_size, 121, dropout_p=dropout_p)
+    myadam_encode = optim.Adam(my_encoderrnn.parameters(), lr=mylr)
+    myadam_decode = optim.Adam(my_attndecoderrnn.parameters(), lr=mylr)
+
+    mse_loss = nn.MSELoss(reduction='sum')
+
+    for epoch_idx in range(1, epochs + 1):
+        train_y_true = []
+        train_y_pre = []
+        test_y_true = []
+        test_y_pre = []
+
+        my_encoderrnn.train()
+        my_attndecoderrnn.train()
+        for train_item, (train_x, train_y) in enumerate(tqdm(train_dataloader), start=1):
+            y_pre, y = Train_Iters(train_x, train_y, my_encoderrnn, my_attndecoderrnn, myadam_encode, myadam_decode, mse_loss)
+
+            train_y_true.extend(y.squeeze().tolist())
+            train_y_pre.extend(y_pre.squeeze().tolist())
+
+        eval_loss = nn.MSELoss()
+        train_rmse_loss = np.sqrt(eval_loss(train_y_true, train_y_pre))
+        
+        print(f'The result of epoch{epoch_idx}:')
+        print("Train RMSELoss:", train_rmse_loss)
+        print("*" * 50)
+
+
 if __name__ == '__main__':
-    dm_test_MyPairsDataset()
+    # dm_test_MyPairsDataset()
+    Train_seq2seq()
